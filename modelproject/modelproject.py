17c0c3414ec1412866fe657c6a386f4d68aba824
zgk915 <zgk915@alumni.ku.dk>
2023-04-30 14:48:58 +0200

    IS-LM model sat op

--- modelproject/modelproject.py
+++ modelproject/modelproject.py
@@ -1,22 +1,83 @@
-from scipy import optimize
+def IS_equation(Y, r, C, T, I, G):
+    """IS curve equation"""
+    return Y - C(Y - T) - I(r) - G
 
-def solve_ss(alpha, c):
-    """ Example function. Solve for steady state k. 
+def LM_equation(Y, r, L):
+    """LM curve equation"""
+    return L(Y, r) - M/P
 
-    Args:
-        c (float): costs
-        alpha (float): parameter
+def I_prime(r):
+    """Derivative of the investment function"""
+    return -50
 
-    Returns:
-        result (RootResults): the solution represented as a RootResults object.
+def L_prime_Y(Y, r):
+    """Partial derivative of the money demand function with respect to output"""
+    return 0.5
 
-    """ 
-    
-    # a. Objective function, depends on k (endogenous) and c (exogenous).
-    f = lambda k: k**alpha - c
-    obj = lambda kss: kss - f(kss)
+def L_prime_r(Y, r):
+    """Partial derivative of the money demand function with respect to interest rate"""
+    return -20
 
-    #. b. call root finder to find kss.
-    result = optimize.root_scalar(obj,bracket=[0.1,100],method='bisect')
-    
-    return result
+def IS_prime_Y(Y, r):
+    """Partial derivative of the IS equation with respect to output"""
+    return 1
+
+def IS_prime_r(Y, r):
+    """Partial derivative of the IS equation with respect to interest rate"""
+    return -1
+
+def LM_prime_Y(Y, r):
+    """Partial derivative of the LM equation with respect to output"""
+    return 1
+
+def LM_prime_r(Y, r):
+    """Partial derivative of the LM equation with respect to interest rate"""
+    return -1
+
+def solve_IS_LM_model(Y_guess, r_guess, C, T, I, G, L, M, P, tol=1e-6, max_iter=100):
+    # Implement the solve_IS_LM function using the Newton-Raphson method
+    def solve_IS_LM(Y_guess, r_guess):
+        # Implement the Newton-Raphson method here
+        Y = Y_guess
+        r = r_guess
+
+        for i in range(max_iter):
+            # Calculate the values of the IS and LM equations and their derivatives
+            IS = IS_equation(Y, r, C, T, I, G)
+            LM = LM_equation(Y, r, L)
+            IS_prime_Y_val = IS_prime_Y(Y, r)
+            IS_prime_r_val = IS_prime_r(Y, r)
+            LM_prime_Y_val = LM_prime_Y(Y, r)
+            LM_prime_r_val = LM_prime_r(Y, r)
+
+            # Calculate the Jacobian matrix
+            J = np.array([[IS_prime_Y_val, IS_prime_r_val], [LM_prime_Y_val, LM_prime_r_val]])
+
+            # Calculate the residuals
+            residuals = np.array([IS, LM])
+
+            # Solve the linear system J * delta = -residuals for delta
+            delta = np.linalg.solve(J, -residuals)
+
+            # Update the values of Y and r
+            Y += delta[0]
+            r += delta[1]
+
+            # Check convergence
+            if np.linalg.norm(delta) < tol:
+                return Y, r
+
+        # If the iteration does not converge within the maximum number of iterations, return None
+        return None, None
+
+    # Call the solve_IS_LM function to get the equilibrium values
+    Y_solution, r_solution = solve_IS_LM(Y_guess, r_guess)
+
+    # Print the equilibrium values
+    if Y_solution is not None:
+        print("Equilibrium output:", Y_solution)
+        print("Equilibrium interest rate:", r_solution)
+
+    # Return the equilibrium values
+    return Y_solution, r_solution
+
